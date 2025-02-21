8f1b004ab6cafa62dc0e3cb31891f920c10affbb
Romain Sanson <romainsanson@MacBook-Pro-de-Romain.local>
2025-02-21 12:58:55 +0100

    index new and create tasks done

--- Gemfile
+++ Gemfile
@@ -43,6 +43,9 @@ group :development, :test do
 
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
+
+  gem 'pry-byebug'
+  gem 'pry-rails'
 end
 
 group :development do
--- lib/domain/use_cases/create_task.rb
+++ lib/domain/use_cases/create_task.rb
@@ -1,30 +1,32 @@
-module UseCases
-  class CreateTask
-    TaskContract = Dry::Schema.Params do
-      required(:title).filled(:string)
-      required(:description).filled(:string)
-      required(:due_date).filled(:date)
-    end
-
-    def initialize(task_repository)
-      @task_repository = task_repository
-      @contract = TaskContract.new
-    end
-
-    def execute(title:, description:, due_date:)
-      validation = @contract.call(title: title, due_date: due_date)
-
-      if validation.failure?
-        return OpenStruct.new(success?: false, errors: validation.errors.to_h)
+module Domain
+  module UseCases
+    class CreateTask
+      TaskContract = Dry::Schema.Params do
+        required(:title).filled(:string)
+        required(:description).filled(:string)
+        required(:due_date).filled(:date)
       end
 
-      task_entity = Entities::Task.new(
-        title: title,
-        description: description,
-        due_date: due_date
-      )
+      def initialize(task_repository)
+        @task_repository = task_repository
+        @contract = TaskContract
+      end
 
-      @task_repository.create(task_entity)
+      def execute(title:, description:, due_date:)
+        validation = @contract.call(title:, description:, due_date:)
+
+        if validation.failure?
+          return OpenStruct.new(success?: false, errors: validation.errors.to_h)
+        end
+
+        task_entity = Entities::Task.new(
+          title: title,
+          description: description,
+          due_date: due_date
+        )
+
+        @task_repository.create(task_entity)
+      end
     end
   end
 end
